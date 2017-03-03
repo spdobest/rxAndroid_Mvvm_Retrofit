1c85c233263dfc07c9c530651d87e69f059b431b
spdobest <sp.dobest@gmail.com>
2017-03-03 13:38:01 +0530

    prasad : minor UI changes

--- app/src/main/java/spinc/mvvmretrofitrxandroid/net/ApiService.java
+++ app/src/main/java/spinc/mvvmretrofitrxandroid/net/ApiService.java
@@ -4,13 +4,11 @@ import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 
 public class ApiService {
+    private static final String ROOT_URL = "https://gist.githubusercontent.com/spdobest/484caddbe902deb3969b31eb0a4f7a16/raw/b39942272671f8df1fe1dfd83fd156ee3195e444";
 
-    //    private static final String FORUM_SERVER_URL = "http://jsonplaceholder.typicode.com";
-    private static final String FORUM_SERVER_URL = "https://gist.githubusercontent.com/spdobest/484caddbe902deb3969b31eb0a4f7a16/raw/b39942272671f8df1fe1dfd83fd156ee3195e444";
-    String url;
     private Api mForumApi;
 
-    public ApiService(boolean isTrue) {
+    public ApiService() {
 
         RequestInterceptor requestInterceptor = new RequestInterceptor() {
             @Override
@@ -18,16 +16,8 @@ public class ApiService {
                 request.addHeader("Accept", "application/json");
             }
         };
-
-        if (!isTrue) {
-            url = FORUM_SERVER_URL;
-        } else {
-            url = FORUM_SERVER_URL;
-        }
-
-
         RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(url)
+                .setEndpoint(ROOT_URL)
                 .setRequestInterceptor(requestInterceptor)
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .build();
